35322c48d8f69beeb736402141e3860f55bf8e03
KlimentSidorov <sidorov_kliment@live.comm>
2022-07-09 21:09:36 +0200

    Nekoj Commit

--- js/main.js
+++ js/main.js
@@ -52,7 +52,6 @@ let increment = (id) => {
 let details = (id) => {
   let search = shopItemsData.find((x) => x.id === id.id);
 
-  console.log(search.id);
   exampleModalLabel.innerHTML = `${search.name}`;
   exampleModalBody.innerHTML = `
   <img class="card-img-top" src=${search.img} alt="test"/>
@@ -77,28 +76,28 @@ let calculation = () => {
 
 calculation();
 
-// let signUp = document.getElementById('signup');
-// let signIn = document.getElementById('sign');
-// let signInPage = document.querySelector('.signinpage');
-// let signInForm = document.querySelector('.signinform');
-// let signUpPage = document.querySelector('.signuppage');
-// let signUpForm = document.querySelector('.signupform');
-// let container1 = document.querySelector('.container-1');
-// let container2 = document.querySelector('.container-2');
+let signUp = document.getElementById('signup');
+let signIn = document.getElementById('sign');
+let signInPage = document.querySelector('.signinpage');
+let signInForm = document.querySelector('.signinform');
+let signUpPage = document.querySelector('.signuppage');
+let signUpForm = document.querySelector('.signupform');
+let container1 = document.querySelector('.container-1');
+let container2 = document.querySelector('.container-2');
 
-// signUp.addEventListener('click', function () {
-//   signInPage.classList.remove('none');
-//   signInForm.classList.add('none');
-//   signUpPage.classList.add('none');
-//   signUpForm.classList.remove('none');
-//   container1.classList.add('bcolor1');
-//   container2.classList.remove('bcolor1');
-// });
-// signIn.addEventListener('click', function () {
-//   signInPage.classList.add('none');
-//   signInForm.classList.remove('none');
-//   signUpForm.classList.add('none');
-//   signUpPage.classList.remove('none');
-//   container2.classList.add('bcolor1');
-//   container1.classList.remove('bcolor1');
-// });
+signUp.addEventListener('click', function () {
+  signInPage.classList.remove('none');
+  signInForm.classList.add('none');
+  signUpPage.classList.add('none');
+  signUpForm.classList.remove('none');
+  container1.classList.add('bcolor1');
+  container2.classList.remove('bcolor1');
+});
+signIn.addEventListener('click', function () {
+  signInPage.classList.add('none');
+  signInForm.classList.remove('none');
+  signUpForm.classList.add('none');
+  signUpPage.classList.remove('none');
+  container2.classList.add('bcolor1');
+  container1.classList.remove('bcolor1');
+});
